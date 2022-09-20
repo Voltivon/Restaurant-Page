084566a2598928dfddc87a02fe04ce8cd0acc23f
Tivon <badboytjy@gmail.com>
2022-09-19 19:57:58 -0700

    Restaurant Page with webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import { initialPageLoad } from "./initialPageLoad.js";
+import { menu } from "./menu.js";
+import { contact } from './contact.js';
+
 console.log("This is my console log statement ha");
+initialPageLoad();
+
+let tabSwitchingModule = (function(){
+    const homeTab = document.querySelector(".home");
+homeTab.addEventListener('click', initialPageLoad);
+
+const menuTab = document.querySelector(".menu");
+menuTab.addEventListener("click", menu);
+
+const contactTab = document.querySelector(".contact");
+contactTab.addEventListener('click', contact);
+})();
+
---
+++ src/initialPageLoad.js
@@ -0,0 +1,31 @@
+export function initialPageLoad(){
+    const contentDiv = document.querySelector("#content");
+
+    contentDiv.replaceChildren();
+
+    const createh1 = document.createElement("h1");
+    createh1.textContent = "Welcome to my restaurant page"
+    createh1.classList.add("header")
+    contentDiv.appendChild(createh1)
+
+    const image = document.createElement('img');
+    image.src = "restaurantPhoto.jpg";
+    image.classList.add("image");
+    contentDiv.appendChild(image);
+
+    const sectionTextOne = document.createElement("p");
+    sectionTextOne.textContent = "Best restaurant in town"
+    sectionTextOne.classList.add("section-text-one");
+    contentDiv.appendChild(sectionTextOne);
+
+    const sectionTextTwo = document.createElement("p");
+    sectionTextTwo.textContent = "Good service";
+    contentDiv.classList.add("section-text-two");
+    contentDiv.appendChild(sectionTextTwo);
+
+    const sectionTextThree = document.createElement("p");
+    sectionTextThree.textContent = "Crazy fun";
+    sectionTextThree.classList.add("section-text-three");
+    contentDiv.appendChild(sectionTextThree);
+
+}
